747c90bde9651cc9711de68e663f4986b5074f1c
sungkuk5420 <sungkuk5420@gmail.com>
2018-04-29 12:55:18 +0900

    remove download limit

--- app.js
+++ app.js
@@ -4,7 +4,7 @@ var favicon = require('serve-favicon');
 var logger = require('morgan');
 var cookieParser = require('cookie-parser');
 var bodyParser = require('body-parser');
-
+var getRawBody = require('raw-body')
 var index = require('./routes/index');
 var users = require('./routes/users');
 
@@ -20,8 +20,21 @@ app.set('view engine', 'ejs');
 // uncomment after placing your favicon in /public
 //app.use(favicon(path.join(__dirname, 'public', 'favicon.ico')));
 app.use(logger('dev'));
-app.use(bodyParser.json());
-app.use(bodyParser.urlencoded({ extended: false }));
+app.use(bodyParser.json({limit: 5000000}));
+app.use(bodyParser.urlencoded({limit: 5000000, extended: true, parameterLimit:50000}));
+app.use(function (err, req, res, next) {
+  console.log(err)
+  getRawBody(req, {
+    length: req.headers['content-length'],
+    limit: '1mb',
+    encoding: contentType.parse(req).parameters.charset
+  }, function (err, string) {
+    console.log(err)
+    if (err) return next(err)
+    req.text = string
+    next()
+  })
+})
 app.use(cookieParser());
 app.use(express.static(path.join(__dirname, 'vue-masonry-plugin-demo-master/dist')));
 
