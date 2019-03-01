e9a8a3ae51963243acd88bd310aaf30556829d53
Sungkuk Kim <sungkuk5420@gmail.com>
2019-03-01 17:50:18 +0900

    구글 애널리틱스 삽입

--- vue-masonry-plugin-demo-master/src/store/ajaxActions.js
+++ vue-masonry-plugin-demo-master/src/store/ajaxActions.js
@@ -1,10 +1,10 @@
 import axios from 'axios'
 export default () => {
   return {
-    searchGoogle (state, cSuccess, cError) {
+    searchGoogle(state, cSuccess, cError) {
       let api = axios.create()
       // let apiURL = `http://localhost:3000/searchGoogle?keyword=${state.searchStr}&count=${state.searchCount}`
-      let apiURL = `http://ec2-54-85-58-222.compute-1.amazonaws.com:8000/searchGoogle?keyword=${state.searchStr}&count=${state.searchCount}&socketId=${state.socketId}`
+      let apiURL = `http://ec2-13-231-53-13.ap-northeast-1.compute.amazonaws.com:8000/searchGoogle?keyword=${state.searchStr}&count=${state.searchCount}&socketId=${state.socketId}`
       axios.all(
         [
           api.get(apiURL)
@@ -17,8 +17,7 @@ export default () => {
           if (errors.length < 1) {
             console.log('200 response= ', responses[0])
             cSuccess(responses[0].data)
-          }
-          else {
+          } else {
             let errmsgs = errors.reduce((memo = '', res) => {
               return memo + `${res.status} : ${res.message}\n`
             }, '')
@@ -28,18 +27,16 @@ export default () => {
         (error) => {
           if (error.response.status === 401) {
             cError(error.response.data)
-          }
-          else if (error.response.status === 500) {
+          } else if (error.response.status === 500) {
             cError(error.response.status)
-          }
-          else {
+          } else {
             console.warn(error)
           }
         }
       )
     },
 
-    downloadImages (state, cSuccess, cError) {
+    downloadImages(state, cSuccess, cError) {
       let api = axios.create()
       let imagesURL = JSON.stringify(state.imagesBlocks)
       console.log(imagesURL)
@@ -53,7 +50,7 @@ export default () => {
             imagesLabels: state.imagesLabels,
             rootDownloadPath: state.rootDownloadPath,
             searchKeyword: state.searchStr,
-            mytoken:state.accessToken
+            mytoken: state.accessToken
           })
         ]
       ).then(
@@ -64,8 +61,7 @@ export default () => {
           if (errors.length < 1) {
             console.log('200 response= ', responses[0])
             cSuccess(responses[0].data)
-          }
-          else {
+          } else {
             let errmsgs = errors.reduce((memo = '', res) => {
               return memo + `${res.status} : ${res.message}\n`
             }, '')
@@ -75,18 +71,16 @@ export default () => {
         (error) => {
           if (error.response.status === 401) {
             cError(error.response.data)
-          }
-          else if (error.response.status === 500) {
+          } else if (error.response.status === 500) {
             cError(error.response.status)
-          }
-          else {
+          } else {
             console.warn(error)
           }
         }
       )
     },
 
-    login (state, cSuccess, cError) {
+    login(state, cSuccess, cError) {
       let api = axios.create()
       // let apiURL = `http://localhost:3000/login?username=${state.username}&password=${state.password}&connectServerType=${state.connectServerType}`
       let apiURL = `http://ec2-54-85-58-222.compute-1.amazonaws.com:8000/login?username=${state.username}&password=${state.password}&connectServerType=${state.connectServerType}`
@@ -102,8 +96,7 @@ export default () => {
           if (errors.length < 1) {
             console.log('200 response= ', responses[0])
             cSuccess(responses[0].data)
-          }
-          else {
+          } else {
             let errmsgs = errors.reduce((memo = '', res) => {
               return memo + `${res.status} : ${res.message}\n`
             }, '')
